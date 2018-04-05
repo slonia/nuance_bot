3baeb530a76558dbc27167d415f7bc29ccec668a
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2018-04-05 22:14:47 +0300

    Allow image upload

--- nuance.go
+++ nuance.go
@@ -3,9 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
+	"math/rand"
+	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"gopkg.in/telegram-bot-api.v4"
 )
@@ -23,7 +29,6 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	} else {
-		fmt.Println(config.Token)
 		bot, err := tgbotapi.NewBotAPI(config.Token)
 		if err != nil {
 			log.Panic(err)
@@ -35,12 +40,55 @@ func main() {
 			if update.Message == nil {
 				continue
 			}
-			if strings.Index(strings.ToLower(update.Message.Text), "нюанс") > -1 || strings.Index(strings.ToLower(update.Message.Text), "ньюанс") > -1 {
-				msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, "./chapaev.jpeg")
+			rand.Seed(time.Now().UTC().UnixNano())
+			if update.Message.Chat.Type == "private" && *update.Message.Photo != nil && len(*update.Message.Photo) > 0 {
+				for i, image := range *update.Message.Photo {
+					if (i+1)%3 == 0 {
+						url, _ := bot.GetFileDirectURL(image.FileID)
+						response, e := http.Get(url)
+						if e != nil {
+							log.Fatal(e)
+						}
+
+						defer response.Body.Close()
+						nameParts := strings.Split(url, "/")
+						name := strconv.Itoa(rand.Int()) + nameParts[len(nameParts)-1]
+						//open a file for writing
+						file, err := os.Create("./images/" + name)
+						if err != nil {
+							log.Fatal(err)
+						}
+						fmt.Println()
+						// Use io.Copy to just dump the response body to the file. This supports huge files
+						_, err = io.Copy(file, response.Body)
+						if err != nil {
+							log.Fatal(err)
+						}
+						file.Close()
+					}
+				}
+			} else if strings.Index(strings.ToLower(update.Message.Text), "нюанс") > -1 || strings.Index(strings.ToLower(update.Message.Text), "ньюанс") > -1 {
+				msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, randomImage())
 				msg.ReplyToMessageID = update.Message.MessageID
 				bot.Send(msg)
 			}
 		}
 	}
+}
 
+func randomImage() string {
+	var files []string
+	fileInfo, err := ioutil.ReadDir("./images/")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for _, file := range fileInfo {
+		name := file.Name()
+		if name[0:1] != "." {
+			files = append(files, name)
+		}
+	}
+	fmt.Println(files)
+	return "./images/" + files[rand.Intn(len(files))]
 }
