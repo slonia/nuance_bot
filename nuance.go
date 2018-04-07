afe5d17b5dfbb622c4596a672b4dec89ed86cdf8
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2018-04-07 10:44:41 +0300

    Add logs

--- nuance.go
+++ nuance.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -20,58 +19,41 @@ type Configuration struct {
 	Token string
 }
 
+var err error
+var bot *tgbotapi.BotAPI
+
 func main() {
 	file, _ := os.Open("config.json")
 	defer file.Close()
 	decoder := json.NewDecoder(file)
 	config := Configuration{}
-	err := decoder.Decode(&config)
-	if err != nil {
-		log.Panic(err)
-	} else {
-		bot, err := tgbotapi.NewBotAPI(config.Token)
-		if err != nil {
-			log.Panic(err)
+	err = decoder.Decode(&config)
+	fatal(err)
+	bot, err = tgbotapi.NewBotAPI(config.Token)
+	fatal(err)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+	fatal(err)
+	for update := range updates {
+		if update.Message == nil {
+			continue
 		}
-		u := tgbotapi.NewUpdate(0)
-		u.Timeout = 60
-		updates, err := bot.GetUpdatesChan(u)
-		for update := range updates {
-			if update.Message == nil {
-				continue
-			}
-			rand.Seed(time.Now().UTC().UnixNano())
-			if update.Message.Chat.Type == "private" && *update.Message.Photo != nil && len(*update.Message.Photo) > 0 {
-				for i, image := range *update.Message.Photo {
-					if (i+1)%2 == 0 {
-						url, _ := bot.GetFileDirectURL(image.FileID)
-						response, e := http.Get(url)
-						if e != nil {
-							log.Fatal(e)
-						}
-
-						defer response.Body.Close()
-						nameParts := strings.Split(url, "/")
-						name := strconv.Itoa(rand.Int()) + nameParts[len(nameParts)-1]
-						//open a file for writing
-						file, err := os.Create("./images/" + name)
-						if err != nil {
-							log.Fatal(err)
-						}
-						fmt.Println()
-						// Use io.Copy to just dump the response body to the file. This supports huge files
-						_, err = io.Copy(file, response.Body)
-						if err != nil {
-							log.Fatal(err)
-						}
-						file.Close()
-					}
+		rand.Seed(time.Now().UTC().UnixNano())
+		text := strings.ToLower(update.Message.Text)
+		log.Println("Processing message", text)
+		if update.Message.Chat.Type == "private" && *update.Message.Photo != nil && len(*update.Message.Photo) > 0 {
+			log.Println("Downloading photo")
+			for i, image := range *update.Message.Photo {
+				if (i+1)%2 == 0 {
+					downloadImage(image.FileID)
 				}
-			} else if strings.Index(strings.ToLower(update.Message.Text), "нюанс") > -1 || strings.Index(strings.ToLower(update.Message.Text), "ньюанс") > -1 {
-				msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, randomImage())
-				msg.ReplyToMessageID = update.Message.MessageID
-				bot.Send(msg)
 			}
+		} else if strings.Index(text, "нюанс") > -1 || strings.Index(text, "ньюанс") > -1 {
+			log.Println("Nuance found. Responding")
+			msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, randomImage())
+			msg.ReplyToMessageID = update.Message.MessageID
+			bot.Send(msg)
 		}
 	}
 }
@@ -79,16 +61,34 @@ func main() {
 func randomImage() string {
 	var files []string
 	fileInfo, err := ioutil.ReadDir("./images/")
-	if err != nil {
-		log.Panic(err)
-	}
-
+	fatal(err)
 	for _, file := range fileInfo {
 		name := file.Name()
 		if name[0:1] != "." {
 			files = append(files, name)
 		}
 	}
-	fmt.Println(files)
+	log.Printf("Selecting random image from %v variants\n", len(files))
 	return "./images/" + files[rand.Intn(len(files))]
+}
+
+func fatal(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func downloadImage(fileId string) {
+	url, _ := bot.GetFileDirectURL(fileId)
+	response, err := http.Get(url)
+	fatal(err)
+	defer response.Body.Close()
+	nameParts := strings.Split(url, "/")
+	name := strconv.Itoa(rand.Int()) + nameParts[len(nameParts)-1]
+	log.Println("Downloading image to", "./images/"+name)
+	file, err := os.Create("./images/" + name)
+	fatal(err)
+	_, err = io.Copy(file, response.Body)
+	fatal(err)
+	file.Close()
 }
