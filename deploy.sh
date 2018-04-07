548bacf67536ee7f2708f700500727a1540025f5
Ilya Kolodnik <ilya.kolodnik@gmail.com>
2018-04-07 10:50:36 +0300

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
 #! /bin/bash
+
+go build nuance.go
+ssh -t berlin@95.85.49.5 "sudo service nuance stop"
+scp ./nuance berlin@95.85.49.5:/home/berlin/nuance_bot/
+scp ./config.json berlin@95.85.49.5:/home/berlin/nuance_bot/config.json
+ssh -t berlin@95.85.49.5 "sudo service nuance start"
+rm nuance
